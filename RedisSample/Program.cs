ba90c827de62f70179f035a3f15dd886d4cc3137
Michael Elly <michael.a.elly@intel.com>
2022-06-01 13:04:36 +0300

    dfg

--- RedisSample/Program.cs
+++ RedisSample/Program.cs
@@ -5,19 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using StackExchange.Redis;
+using System.Text.RegularExpressions;
 
 namespace RedisSample {
 	class Program {
         private static int mCacheItems = 1000;
+        // Split a string that has white spaces, unless they are enclosed within "quotes"?
+        private static Regex re = new Regex(@"['].+?[']|[^ ]+");
 
         static void Main(string[] args) {
             var program = new Program();
             WriteHelp();
             while (true) {
                 Thread.Sleep(500);
-                Console.Write("Enter Selection: ");
+                Console.Write("> ");
                 string line = Console.ReadLine();
-                switch (line.ToLower()) {
+                string[] s = re.Matches(line).Cast<Match>().Select(m => m.Value.Trim('\'')).ToArray();
+                string c = s[0].ToLower();
+
+                switch (c) {
                     case "q":
                     case "x":
                         return;
@@ -33,6 +39,50 @@ namespace RedisSample {
                     case "p":
                         program.Publish();
                         continue;
+                    case "w":
+                        if (s.Length == 3) {
+                            program.WriteKeyValue(s[1], s[2]);
+                        } else if (s.Length == 4 && int.TryParse(s[3], out _)) {
+                            program.WriteKeyValue(s[1], s[2], int.Parse(s[3]));
+                        } else {
+                            Console.WriteLine("Invalid argument. Type h for help. ");
+                        }                        
+                        continue;
+                    case "r":
+                        if (s.Length == 2) {
+                            string rt = program.ReadKeyValue(s[1]);
+                            Console.WriteLine($"Return value: \"{rt ?? "NULL"}\"");
+                        } else {
+                            Console.WriteLine("Invalid argument. Type h for help. ");
+                        }
+                        continue;
+                    case "d":
+                        if (s.Length == 2) {
+                            bool rt = program.DeleteKey(s[1]);
+                            Console.WriteLine($"{(rt ? $"Deleted Key {s[1]}" : $"Key {s[1]} does not exist.")}");
+                        } else {
+                            Console.WriteLine("Invalid argument. Type h for help. ");
+                        }
+                        continue;
+                    case "sw":
+                        if (s.Length == 3) {
+                            program.SetWriteKeyValue(s[1], s[2]);
+                        } else {
+                            Console.WriteLine("Invalid argument. Type h for help. ");
+                        }
+                        continue;
+                    case "sg":
+                        if (s.Length == 2) {
+                            List<string> rt = program.SetGetKeyValues(s[1]);
+                            if (rt.Count > 0) {
+                                Console.WriteLine($"Return value: \r\n{rt.Aggregate((i, j) => i + "\r\n" + j)}");
+                            } else {
+                                Console.WriteLine($"Returns an empty set of values.");
+                            }
+                        } else {
+                            Console.WriteLine("Invalid argument. Type h for help. ");
+                        }
+                        continue;
                     default:
                         Console.WriteLine("Invalid Selection...");
                         WriteHelp();
@@ -42,10 +92,28 @@ namespace RedisSample {
         }
 
         public static void WriteHelp() {
-            Console.WriteLine("Select option:");
-            Console.WriteLine("  c Sample cache        s Subscribe");
-            Console.WriteLine("  s Subscribe           p Publish");
+            //Console.WriteLine("Select option:");
+            //Console.WriteLine("  w Write cache         s Subscribe");
+            //Console.WriteLine("  r Read cache          p Publish");
+            //Console.WriteLine("  q Quit                h Help");
+            //Console.WriteLine();
+
+            Console.WriteLine("Writing/Reading key/value:");
+            Console.WriteLine("  w  '<key>' '<value>' <timeout sec>");
+            Console.WriteLine("  r  '<key>'");            
+
+            Console.WriteLine();
+            Console.WriteLine("Writing/Getting values into a sorted set by key:");
+            Console.WriteLine("  sw '<key>' '<value to add into the set>'");
+            Console.WriteLine("  sg '<key>'");
+
+            Console.WriteLine(); 
+            Console.WriteLine("Delete key:");
+            Console.WriteLine("  d '<key>'");
+
+            Console.WriteLine();
             Console.WriteLine("  q Quit                h Help");
+
         }
 
         public void SampleChache(Program program) {
@@ -93,8 +161,54 @@ namespace RedisSample {
 
             for (int i = 1; i < devicesCount; i++) {
                 var value = rnd.Next(0, 10000);
-                cache.StringSet($"Device_Status:{i}", value);                
+                cache.StringSet($"Device_Status:{i}", value);
             }
         }
+
+        // write / read / delete string values
+        public void WriteKeyValue(string key, string value, int timeoutSec = -1) {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            if (timeoutSec == -1) timeoutSec = 5 * 60;
+            cache.StringSet(key, value, TimeSpan.FromSeconds(timeoutSec));
+        }
+        public string ReadKeyValue(string key) {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            var val = cache.StringGet(key);
+            return val.HasValue ? val.ToString() : "";
+        }
+        public bool DeleteKey(string key) {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            //var val = cache.StringGetDelete(key);
+            //return val == RedisValue.Null ? false : true;
+            return cache.KeyDelete(key);            
+        }
+
+        public static DateTime mRefDate = new DateTime(2000, 1, 1);
+
+        // write / read / delete string sets per key
+        public void SetWriteKeyValue(string key, string value) {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            try {
+                cache.SortedSetAdd(key, new RedisValue(value), (DateTime.Now - mRefDate).TotalSeconds, CommandFlags.None);
+            } catch (StackExchange.Redis.RedisServerException ex) {
+                Console.WriteLine($"Error: key '{key}' is already used by other type. {ex.Message}");
+			} catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        public List<string> SetGetKeyValues(string key) {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            List<string> l = new List<string>();
+            while (true) {
+                var v = cache.SortedSetPop(key);
+                if (v != null) {
+                    l.Add(v.Value.Element.ToString());
+                } else {
+                    break;
+                }
+            }
+            return l;
+        }
+
     }
 }
